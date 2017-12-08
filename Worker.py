843711fd5c0cc751f2d0346acd8f079028d5bac5
Hanut <mathurh@tcd.ie>
2017-12-08 01:32:29 +0000

    Completed acquiring git repo URL from Manager

--- Worker.py
+++ Worker.py
@@ -14,8 +14,15 @@ def execute():
     reqURL = 'http://' + ip + '/' + port
     getURL = reqURL + '/repo'
     cycURL = reqURL + '/cyclo'
+    getCommit = requests.get(getURL,json = {'pullStatus' : True})
+    commitData = json.loads(getCommit.text)
+    repoURL = commitData['repo']
+    ## Pull git repo to local for evaluation
     
+    ##Notify manager of pull success
+    _ = requests.get(getURL,json = {'pullStatus' : False})
     
+    ## GET tasks from getrepo completed
     
     
 if __name__ == '__main__':
